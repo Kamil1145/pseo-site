92caf36a89195543f6d54d136d98657cd5ab5dfe
Kamil Abram <kamil.abram@vp.pl>
2026-08-05 21:27:40 +0200

    1

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,7 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://devsalaries.co', // Kluczowe: pełny URL z https://
+  integrations: [sitemap()],
+});
