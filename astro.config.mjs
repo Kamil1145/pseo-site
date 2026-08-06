85bd842647fe09f7d73e435625f025aaa14d5131
Kamil Abram <kamil.abram@vp.pl>
2026-08-06 22:38:04 +0200

    fixes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,13 @@
 import { defineConfig } from 'astro/config';
-import sitemap from '@astrojs/sitemap';
+import tailwindcss from '@tailwindcss/vite';
+import sitemap from '@astrojs/sitemap'; // <--- DODAJ TEN IMPORT
 
 export default defineConfig({
-  site: 'https://devsalaries.co', // Kluczowe: pełny URL z https://
-  integrations: [sitemap()],
+  site: 'https://devsalaries.co', // Upewnij się, że masz tu wpisany swój adres
+  integrations: [
+    sitemap(),
+  ],
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
